4d5e61b42ca4a7ecc5d695aef2592564b29ef7a4
Cristian Cappo <ccappopy@gmail.com>
2023-05-17 11:39:47 -0400

    Prueba

---
+++ Proyecto/src/main/java/org/secapp/TestServlet.java
@@ -0,0 +1,55 @@
+package org.secapp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		
+		String [] param1 = request.getParameterValues("valor");
+		
+		if ( param1 != null ) 
+		  	pw.println("<html><body><em>XSS</em><h2>Param=" + param1[0] + "</h2></body></html>");
+		else
+			pw.println("<html><body><h2>SIN PARAMETROS</h2></body></html>");
+		
+		pw.close();
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
+
+
+
